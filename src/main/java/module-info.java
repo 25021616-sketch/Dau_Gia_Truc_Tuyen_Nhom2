2bfb6100e1392f9c3f16e910cf692714c7381087
minhnguyen0107 <minhht0107@gmail.com>
2026-05-18 16:15:54 +0700

    feat: sửa chữa cần thiết

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module Team2_CS2_Auction {
     requires java.net.http;
     requires com.google.gson;
     requires java.desktop;
+    requires org.flywaydb.core;
 
     // 2. Cho phép JavaFX (FXML) truy cập vào Controller để bắt sự kiện (Button click,...)
     opens Team2_CS2_Auction.Controller to javafx.fxml;
@@ -26,4 +27,5 @@ module Team2_CS2_Auction {
     opens Team2_CS2_Auction.Model.user to com.google.gson, javafx.base;
     opens Team2_CS2_Auction.Model.auction to com.google.gson, javafx.base;
     opens Team2_CS2_Auction.Networking to com.google.gson;
+    opens db.migration to org.flywaydb.core;
 }
